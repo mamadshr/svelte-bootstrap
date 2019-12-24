db6e6181a8b1251c5139509c38a147e8b26ba13a
Mohammad Shiravand <shiravandmammad@gmail.com>
2019-12-24 17:43:50 +0330

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name;
+</script>
+
+<div class="container">
+	<h1>Hello {name}!</h1>
+</div>
+
+<style lang="scss">
+	$color: purple;
+
+	@import "../node_modules/bootstrap/scss/bootstrap.scss";
+
+	h1 {
+		color: $color;
+	}
+</style>
